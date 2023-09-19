c8f828116e6070cbec52073edf11be0707bbab09
Harshavardhan BV <bv_harshavardhan@live.com>
2023-09-19 19:25:10 +0530

    Added optimization part to model

--- Pop_Model.py
+++ Pop_Model.py
@@ -30,35 +30,43 @@ def PK_model(t, y, a):
     dose_cent, dose_peri = y
     cr, icr, cv, pv = a
     # Equations are wrong need to change
-    dy1 = dose_cent*cr - dose_cent*icr + dose_peri*icr
+    dy1 = - dose_cent*cr - dose_cent*icr + dose_peri*icr
     dy2 = icr*dose_cent - icr*dose_peri
     return np.array([dy1, dy2])
 #%%
 # Wrapper to solve ODE for each agent
 def sol_wrap(a):
+    *a, dose = a
     return solve_ivp(PK_model, (0, t_max), [dose, 0], args=(a,), dense_output=True)
 #%%
 # Define Force of Infection
 def FOI(t):
+    # yearly seasonality
+    t = t % 365
+    # Given in problem statement
     BL = 2.25
     A = 2633
     sig = 48
     mu = 162
-    # Have to find some way to get probability of infection
     return BL + A * np.exp(-((t - mu)**2)/(2*sig**2))/(sig*np.sqrt(2*np.pi))
+#%% 
+# Probability of infection
+def P_inf(t):
+    # Assuming a constant per capita rate of infection within dt
+    return 1-np.exp(-FOI(t)/n_agent)
 #%%
 def infect(dose, infected, t):
     if infected:
         # If infected stay infected
         return 1
-    elif dose > min_dose:
+    elif dose >= min_dose:
         # If dose high stay uninfected
         return 0
     else:
         # get random number
         r = np.random.random()
         # If random number < FOI, become infected
-        if r < FOI(t):
+        if r < P_inf(t):
             return 1
         else:
             return 0
@@ -69,7 +77,8 @@ def simulation(dose, t_d):
     icr = np.random.normal(icr_mu, icr_sigma, n_agent)
     cv = np.random.normal(cv_mu, cv_sigma, n_agent)
     pv = np.random.normal(pv_mu, pv_sigma, n_agent)
-    gothamite_param = np.array([cr, icr, cv, pv]).T
+    dosage = np.full(n_agent, dose)
+    gothamite_param = np.array([cr, icr, cv, pv,dosage]).T
     # Make n_agent solution objects for the ODE with parallel
     with Pool() as pool:
         gothamite_sol = pool.map(sol_wrap, gothamite_param)
@@ -86,4 +95,17 @@ def simulation(dose, t_d):
     # get the number infected 
     return gothamite_infected.sum()
 # %%
-# To do: run replicates and find way to optimize
+def simulate_replicates(dose, t_d, n_rep):
+    return [simulation(dose, t_d) for i in range(n_rep)]
+# %%
+# Find optimal dose by running simulation for different doses and time of dose
+dose = 10
+while dose > 0:
+    sim_rep = simulate_replicates(dose, 30, 10)
+    print(dose, sim_rep)
+    if (sim_rep>0.75*n_agent).all():
+        break
+    dose -= 1
+for i in range(0,365,7):
+    sim_rep = simulate_replicates(dose, i, 10)
+    print(i, sim_rep)
